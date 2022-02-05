585d7d9a8ae817ccbed689c3577117457856c22e
n0ring <98221398+n0ring@users.noreply.github.com>
2022-02-05 15:39:29 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,17 +8,18 @@
 #define STD_IN	0
 #define STD_ERR 2
 
-#define SIDE_OUT 0
-#define SIDE_IN 1
-
 #define TYPE_END 0
 #define TYPE_PIPE 1
 #define TYPE_BREAK 2
 
+#define ERR_FATAL "error: fatal"
+#define CD_ERR_ARG "error: cd: bad arguments"
+#define CD_ERR_CHDIR "error: cd: cannot change directory to"
+#define EXEC_ERR "error: cannot execute"
+
 typedef struct s_list
 {
 	char			**args;
-	int				length;
 	int				type;
 	int				pipes[2];
 	struct s_list	*next;
@@ -34,32 +35,36 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
+void	show_err(char *msg, char *arg)
+{
+	write(STD_ERR, msg, ft_strlen(msg));
+	if (arg)
+	{
+		write(STD_ERR, " ", 1);
+		write(STD_ERR, arg, ft_strlen(arg));
+	}
+	write(STD_ERR, "\n", 1);
+}
+
 void clear_list(t_list *list)
 {
 	t_list *tmp;
 	char	**str_p;
 	
-	// printf("CLEAR\n");
 	while (list)
 	{
 		tmp = list;
 		str_p = list->args;
-		while (str_p && *str_p)
-		{
-			// printf("arg: %s\n", *str_p);
-			str_p++;
-		}
 		if (list->args)
 			free(list->args);
-		// printf("block type %s %d\n", list->type == TYPE_BREAK ? "TYPE_BREAK" : "TYPE_PIPE", list->type);
 		list = tmp->next;
 		free(tmp);
 	}
 }
 
-void exit_fatal(char *msg)
+void exit_fatal()
 {
-	write(STD_ERR, msg, ft_strlen(msg));
+	show_err(ERR_FATAL, NULL);
 	exit (EXIT_FAILURE);
 }
 
@@ -87,6 +92,23 @@ int get_cmds_in_block(char **arg, int i)
 	return (res);
 }
 
+t_list *get_new_el(int cmds)
+{
+	t_list *new_el;
+
+	new_el = malloc(sizeof(t_list));
+	if (!new_el)
+		exit_fatal();
+	new_el->args = malloc((cmds + 1) * sizeof(char *));
+	if (!new_el->args)
+		exit_fatal();
+	(new_el->args)[cmds] = NULL;
+	new_el->type = TYPE_END;
+	new_el->pipes[0] = -2;
+	new_el->pipes[1] = -2;
+	return (new_el);
+}
+
 void	parse_args(t_list **list,  char **arg, int *i)
 {
 	int			inner_i;
@@ -98,11 +120,10 @@ void	parse_args(t_list **list,  char **arg, int *i)
 
 	if (!strcmp(arg[inner_i], ";") || !strcmp(arg[inner_i], "|"))
 	{
-		// printf("IN ;| branch\n");
 		if (*list)
 		{
 			current_el = to_last_el(*list);
-			current_el->type = (!strcmp(arg[inner_i], ";")) ? TYPE_BREAK : TYPE_PIPE;
+			current_el->type = (!strcmp(arg[inner_i], "|")) ? TYPE_PIPE : TYPE_END;
 		}
 		(*i)++;
 		return ;
@@ -113,14 +134,7 @@ void	parse_args(t_list **list,  char **arg, int *i)
 		(*i)++;
 		return ;
 	}
-	current_el = malloc(sizeof(t_list));
-	if (!current_el)
-		exit_fatal("error: fatal\n");
-	current_el->args = malloc((cmds_in_block + 1) * sizeof(char *));
-	if (!current_el->args)
-		exit_fatal("error: fatal\n");
-	(current_el->args)[cmds_in_block] = NULL;
-	// printf("cmd in block %d\n", cmds_in_block);
+	current_el = get_new_el(cmds_in_block);
 	*i = *i + cmds_in_block;
 	k = 0;
 	while (k < cmds_in_block)
@@ -133,9 +147,6 @@ void	parse_args(t_list **list,  char **arg, int *i)
 		to_last_el(*list)->next = current_el;
 	else
 		*list = current_el;
-	current_el->type = TYPE_END;
-	current_el->pipes[0] = -2;
-	current_el->pipes[1] = -2;
 }
 
 int cd_builtin(t_list *el)
@@ -147,14 +158,12 @@ int cd_builtin(t_list *el)
 		args_count++;
 	if (args_count != 2)
 	{
-		write(STD_ERR, "error: cd: bad arguments\n", ft_strlen("error: cd: bad arguments\n"));
+		show_err(CD_ERR_ARG, NULL);
 		return (EXIT_FAILURE);
 	}
 	if (chdir(el->args[1]))
 	{
-		write(STD_ERR, "error: cd: cannot change directory to ", ft_strlen("error: cd: cannot change directory to "));
-		write(STD_ERR, el->args[1], ft_strlen(el->args[1]));
-		write(STD_ERR, "\n", 1);
+		show_err(CD_ERR_CHDIR, el->args[1]);
 		return (EXIT_FAILURE);
 	}	
 	return (EXIT_SUCCESS);
@@ -168,10 +177,13 @@ int exec(t_list *el, int input_fd, char **env)
 
 	last_exit_code = 0;
 	if (el->type == TYPE_PIPE)
-		pipe(el->pipes);
+	{
+		if (pipe(el->pipes) == -1)
+			exit_fatal();
+	}
 	pid = fork();
 	if (pid == -1)
-		exit_fatal("error: fatal\n");
+		exit_fatal();
 	if (pid == 0)
 	{
 		if (el->type == TYPE_PIPE)
@@ -179,9 +191,9 @@ int exec(t_list *el, int input_fd, char **env)
 		if (input_fd != -1)
 			dup2(input_fd, STD_IN);
 		execve(el->args[0], el->args, env);
-		write(2, "error: cannot execute ", ft_strlen("error: cannot execute "));
-		write(2, el->args[0], ft_strlen(el->args[0]));
-		write(2, "\n", 1);
+		show_err(EXEC_ERR, el->args[0]);
+		if (el->type == TYPE_PIPE)
+			close(el->pipes[1]);
 		exit(1);
 	}
 	else
@@ -191,7 +203,6 @@ int exec(t_list *el, int input_fd, char **env)
 		waitpid(pid, &exit_status, 0);
 		if (WIFEXITED(exit_status))
 			last_exit_code = WEXITSTATUS(exit_status);
-		printf("exit code %d\n", last_exit_code);
 		if (input_fd != -1)
 			close(input_fd);
 	}
@@ -224,8 +235,6 @@ int exec_part(t_list *list, char **env)
 	}
 	return (input_fd);
 }
-
-
 
 int main(int argc, char **argv, char **env)
 {
